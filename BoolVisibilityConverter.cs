ff64dee529ea4102047768ba65b232d148d78448
Michal Hajduk <michal.hajudk@gmail.com>
2020-03-09 23:12:37 +0100

    Add project files.

---
+++ BoolVisibilityConverter.cs
@@ -0,0 +1,30 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Windows.Data;
+using System.Threading.Tasks;
+using System.Globalization;
+using System.Windows;
+
+namespace UsersPanel
+{
+    [ValueConversion(typeof(bool), typeof(Visibility))]
+    class BoolVisibilityConverter : IValueConverter
+    {
+        public object Convert(object value, Type targetType, object parameter, CultureInfo culture)
+        {
+            bool isVisible = (bool)value;
+
+            if (isVisible)
+                return Visibility.Visible;
+            else
+                return Visibility.Collapsed;
+        }
+
+        public object ConvertBack(object value, Type targetType, object parameter, CultureInfo culture)
+        {
+            throw new NotImplementedException();
+        }
+    }
+}
---
+++ MainWindow.xaml.cs
@@ -0,0 +1,34 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Navigation;
+using System.Windows.Shapes;
+
+namespace UsersPanel
+{
+    /// <summary>
+    /// Interaction logic for MainWindow.xaml
+    /// </summary>
+    public partial class MainWindow : Window
+    {
+        public MainWindow()
+        {
+            InitializeComponent();
+        }
+
+        private void Log_in(object sender, RoutedEventArgs e)
+        {
+            if (inUsernameBox.Text == "" || inPasswordBox.Password == "")
+                MessageBox.Show("Enter all data.", "Warning", MessageBoxButton.OK);
+        }
+    }
+}
